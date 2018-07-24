600ce5e437bf07241ca56436bc0a51a884be5d1c
Mangiang <mangiang@orange.fr>
2018-07-24 21:27:58 +0200

    update backend api address

--- src/server.js
+++ src/server.js
@@ -22,22 +22,22 @@ app.use(express.static('public'));
 
 function InitRoutes(dbo){
     // Login
-    app.post('/user/login', (req, res) => userRoute.Login(req,res, dbo));
+    app.post('/api/user/login', (req, res) => userRoute.Login(req,res, dbo));
     // Project
-    app.get('/project/list', (req, res) => projectRoute.GetAllProjects(req, res, dbo));
-    app.get('/project/detail/:id', (req, res) => projectRoute.GetProjectDetail(req, res, dbo));
-    app.post('/project/create', (req, res) => VerifyToken(req, res, dbo, projectRoute.CreateProject));
-    app.put('/project/update/:id', (req, res) => VerifyToken(req, res, dbo, projectRoute.UpdateProject));
-    app.delete('/project/delete/:id', (req, res) => VerifyToken(req, res, dbo, projectRoute.DeleteProject));
+    app.get('/api/project/list', (req, res) => projectRoute.GetAllProjects(req, res, dbo));
+    app.get('/api/project/detail/:id', (req, res) => projectRoute.GetProjectDetail(req, res, dbo));
+    app.post('/api/project/create', (req, res) => VerifyToken(req, res, dbo, projectRoute.CreateProject));
+    app.put('/api/project/update/:id', (req, res) => VerifyToken(req, res, dbo, projectRoute.UpdateProject));
+    app.delete('/api/project/delete/:id', (req, res) => VerifyToken(req, res, dbo, projectRoute.DeleteProject));
     //Image project
-    app.post('/project/upload/:id', (req, res) => VerifyToken(req, res, dbo, projectRoute.UploadImage));
-    app.delete('/project/deleteImage/:id', (req, res) => VerifyToken(req, res, dbo, projectRoute.DeleteImage));
+    app.post('/api/project/upload/:id', (req, res) => VerifyToken(req, res, dbo, projectRoute.UploadImage));
+    app.delete('/api/project/deleteImage/:id', (req, res) => VerifyToken(req, res, dbo, projectRoute.DeleteImage));
     // Timeline date
-    app.get('/timeline/list', (req, res) => timelineRoute.GetAllTimelines(req, res, dbo));
-    app.get('/timeline/detail/:id', (req, res) => timelineRoute.GetTimelineDetail(req, res, dbo));
-    app.post('/timeline/create', (req, res) => VerifyToken(req, res, dbo, timelineRoute.CreateTimeline));
-    app.put('/timeline/update/:id', (req, res) => VerifyToken(req, res, dbo, timelineRoute.UpdateTimeline));
-    app.delete('/timeline/delete/:id', (req, res) => VerifyToken(req, res, dbo, timelineRoute.DeleteTimeline))
+    app.get('/api/timeline/list', (req, res) => timelineRoute.GetAllTimelines(req, res, dbo));
+    app.get('/api/timeline/detail/:id', (req, res) => timelineRoute.GetTimelineDetail(req, res, dbo));
+    app.post('/api/timeline/create', (req, res) => VerifyToken(req, res, dbo, timelineRoute.CreateTimeline));
+    app.put('/api/timeline/update/:id', (req, res) => VerifyToken(req, res, dbo, timelineRoute.UpdateTimeline));
+    app.delete('/api/timeline/delete/:id', (req, res) => VerifyToken(req, res, dbo, timelineRoute.DeleteTimeline))
 }
 
 function VerifyToken(req, res, dbo, callback)
